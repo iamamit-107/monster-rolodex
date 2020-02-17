5e5d76826c0b577ae1a5d63024ffc4d216aa409d
iamamit-107 <ahmed.amit107@gmil.com>
2020-02-17 20:59:10 +0600

    Updates

---
+++ precache-manifest.38a921bb2cab2b40f06be5df9b63522e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afc38674d176a4ca0dc8a9dbc5387b7",
+    "url": "/new3/index.html"
+  },
+  {
+    "revision": "3b1578fe94dbcd5f8de6",
+    "url": "/new3/static/css/main.b6be2f9e.chunk.css"
+  },
+  {
+    "revision": "c7b2224723b4875862af",
+    "url": "/new3/static/js/2.82b201ab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/new3/static/js/2.82b201ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1578fe94dbcd5f8de6",
+    "url": "/new3/static/js/main.f45b98f4.chunk.js"
+  },
+  {
+    "revision": "1bcae7678fd325aa059b",
+    "url": "/new3/static/js/runtime-main.3afe5ab5.js"
+  }
+]);
